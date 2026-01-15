3d1d4a0dc3805cfc3cebeebe971dc91a8e50a86a
david leonardo lamprea segura <david@davids-MacBook-Pro.local>
2026-01-14 20:55:13 -0500

    notificaciones activas

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -30,4 +31,12 @@ export default defineConfig({
       }
     })
   ],
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+    host: 'localhost',
+    port: 4000
+  }
 })
